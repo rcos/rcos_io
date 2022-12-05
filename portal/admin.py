084f5470c957f59210e98b81529aad047e3bcd22
Frank Matranga <thefrankmatranga@gmail.com>
2022-12-05 03:21:13 -0500

    Add more admin models

--- portal/admin.py
+++ portal/admin.py
@@ -3,11 +3,51 @@ from django.contrib import admin
 from .models import *
 
 admin.site.site_header = "RCOS IO Administration"
-# admin.site.site_title = ""
 
-# Register your models here.
+# Inlines
+class EnrollmentInline(admin.TabularInline):
+    model = Enrollment
+    extra = 1
 
-admin.site.register(Semester)
-admin.site.register(User)
-admin.site.register(Project)
-admin.site.register(Enrollment)
+class MeetingAttendanceInline(admin.TabularInline):
+    model = MeetingAttendance
+    extra = 1
+
+# Model Admins
+
+class SemesterAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["id", "name"]}),
+        ("Dates", {"fields": ["start_date", "end_date"]}),
+        (None, {"fields": ["is_accepting_new_projects"]})
+    ]
+    list_display = ("name", "start_date", "end_date", "enrollment_count", "project_count")
+    search_fields = ["name"]
+    inlines = [EnrollmentInline]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("is_approved", "full_name", "role", "email")
+    search_fields = ("first_name", "last_name", "email", "rcs_id")
+    list_filter = ("role", "is_approved", "enrollments__semester__name")
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "tagline")
+    search_fields = ("name", "tagline")
+    list_filter = ("is_approved", "enrollments__semester__name")
+    inlines = [EnrollmentInline]
+
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ("semester", "user", "project")
+    list_filter = ("semester__name", "credits", "is_for_pay", "is_project_lead", "is_coordinator")
+
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ("is_published", "name", "type", "starts_at")
+    search_fields = ("name", "type")
+    list_filter = ("starts_at", "type", "is_published")
+    inlines = [MeetingAttendanceInline]
+
+admin.site.register(Semester, SemesterAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Enrollment, EnrollmentAdmin)
+admin.site.register(Meeting, MeetingAdmin)
--- portal/models.py
+++ portal/models.py
@@ -19,17 +19,26 @@ class Semester(TimestampedModel):
         max_length=30, help_text="User-facing name of semester, e.g. Fall 2022"
     )
     is_accepting_new_projects = models.BooleanField(
+        "accepting new projects?",
         default=False,
         help_text="Whether new projects can be proposed for this semester",
     )
     start_date = models.DateField(
-        "start of semester",
+        "first day",
         help_text="The first day of the semester according to the RPI Academic Calendar: https://info.rpi.edu/registrar/academic-calendar",
     )
     end_date = models.DateField(
-        "end of semester",
+        "last day",
         help_text="The last day of the semester according to the RPI Academic Calendar: https://info.rpi.edu/registrar/academic-calendar",
     )
+
+    @property
+    def enrollment_count(self):
+        return self.enrollments.count()
+
+    @property
+    def project_count(self):
+        return "???"
 
     def __str__(self) -> str:
         return self.name
@@ -40,7 +49,7 @@ class User(TimestampedModel):
     EXTERNAL = "external"
     ROLE_CHOICES = ((RPI, "RPI"), (EXTERNAL, "External"))
 
-    is_approved = models.BooleanField(default=False)
+    is_approved = models.BooleanField("approved?", default=False)
     role = models.CharField(choices=ROLE_CHOICES, max_length=30)
 
     # Profile
@@ -73,6 +82,14 @@ class User(TimestampedModel):
     github_username = models.CharField(
         blank=True, max_length=200, help_text="The user's GitHub username (not user ID)"
     )
+
+    @property
+    def full_name(self):
+        return (self.first_name + " " + self.last_name).strip()
+
+    @property
+    def is_setup(self):
+        return self.first_name and self.last_name and self.github_username and self.discord_user_id
 
     def __str__(self) -> str:
         return self.email
@@ -109,7 +126,7 @@ class Project(TimestampedModel):
     )
 
     # Relationships
-    enrollments = models.ManyToManyField(User, through="Enrollment")
+    # enrollments = models.ManyToManyField(User, through="Enrollment")
 
     def __str__(self) -> str:
         return self.name
@@ -120,22 +137,24 @@ class Project(TimestampedModel):
 
 
 class Enrollment(TimestampedModel):
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name="enrollments")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="enrollments")
     project = models.ForeignKey(
-        Project, null=True, blank=True, on_delete=models.SET_NULL
+        Project, null=True, blank=True, on_delete=models.SET_NULL,
+        related_name="enrollments"
     )
     credits = models.IntegerField(
         default=0,
         help_text="How many course credits the user is participating in RCOS for this semester. 0 means just for experience.",
     )
     is_for_pay = models.BooleanField(
+        "for pay?",
         default=False,
         help_text="Whether the user is participating in RCOS for pay instead of course credit",
     )
-    is_project_lead = models.BooleanField(default=False)
-    is_faculty_advisor = models.BooleanField(default=False)
-    is_faculty_advisor = models.BooleanField(default=False)
+    is_project_lead = models.BooleanField("project lead?", default=False)
+    is_coordinator = models.BooleanField("coordinator?", default=False)
+    is_faculty_advisor = models.BooleanField("faculty advisor?", default=False)
 
     def __str__(self) -> str:
         return f"{self.semester.name} - {self.user} - {self.project or 'No project'}"
@@ -143,3 +162,41 @@ class Enrollment(TimestampedModel):
     class Meta:
         ordering = ["semester"]
         get_latest_by = ["semester"]
+
+class Meeting(TimestampedModel):
+    SMALL_GROUP = "small_group"
+    LARGE_GROUP = "large_group"
+    WORKSHOP = "workshop"
+    MENTOR = "mentor"
+    COORDINATOR = "coordinator"
+    TYPE_CHOICES = (
+        (SMALL_GROUP, "Small Group"),
+        (LARGE_GROUP, "Large Group"),
+        (WORKSHOP, "Workshop"),
+        (MENTOR, "Mentor"),
+        (COORDINATOR, "Coordinator")
+    )
+
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name="meetings")
+    name = models.CharField(max_length=100, blank=True, help_text="The optional title of the meeting")
+    host = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, help_text="Optional host for the meeting (e.g. mentor hosting a workshop")
+    type = models.CharField(choices=TYPE_CHOICES, max_length=100)
+    is_published = models.BooleanField("published?", default=False, help_text="Whether the meeting is visible to users")
+    starts_at = models.DateTimeField(help_text="When the meeting starts")
+    ends_at = models.DateTimeField(help_text="When the meeting ends")
+    location = models.CharField(max_length=500, blank=True, help_text="Where the meeting takes place either physically or virtually")
+    description_markdown = models.TextField(max_length=10000, blank=True, help_text="Optional publicly displayed description for the meeting. Supports Markdown.")
+
+    # Relationships
+    attendances = models.ManyToManyField(User, through="MeetingAttendance", related_name="meeting_attendances")
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.get_type_display()} - {self.starts_at.strftime('%a %b %-d %Y @ %-I:%M %p')}" 
+
+    class Meta:
+        ordering = ["starts_at"]
+
+class MeetingAttendance(TimestampedModel):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_added_by_admin = models.BooleanField(default=False, help_text="Whether this attendance was added by an admin instead of by the user")
