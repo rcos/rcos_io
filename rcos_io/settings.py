e2411c5adc67fd4c8deb6dbdcfe53da460b932f2
Frank Matranga <thefrankmatranga@gmail.com>
2022-12-05 01:06:53 -0500

    Add portal app

--- rcos_io/settings.py
+++ rcos_io/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "rcos_io.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ["PGPORT"]
     }
 }
 
