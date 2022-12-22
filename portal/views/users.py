2aee45393669d3b4c387f331339b2ba8f6eca2ff
Frank Matranga <thefrankmatranga@gmail.com>
2022-12-22 00:08:42 -0500

    Add users search

--- portal/views/users.py
+++ portal/views/users.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from . import SemesterFilteredListView, SemesterFilteredDetailView
 from ..models import User
 
@@ -9,6 +10,28 @@ class UserIndexView(SemesterFilteredListView):
     # Default to all active RPI members
     queryset = User.objects.filter(is_active=True, is_approved=True, role="rpi")
     semester_filter_key = "enrollments__semester"
+
+    def get_queryset(self):
+        """Apply filters (semester is already handled)"""
+        queryset = super().get_queryset()
+
+        self.search = self.request.GET.get("search")
+        if self.search:
+            queryset = queryset.annotate(
+                search=SearchVector(
+                    "first_name",
+                    "last_name",
+                    "rcs_id",
+                    "graduation_year"
+                ),
+            ).filter(search=self.search)
+
+        return queryset.distinct()
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data["search"] = self.search
+        return data
 
 
 class UserDetailView(SemesterFilteredDetailView):
