c462e5730e009b92c9f2802c96dea79cfa3f9651
Frank Matranga <thefrankmatranga@gmail.com>
2022-12-24 00:17:51 -0500

    Frontpage redesign

--- portal/views/index.py
+++ portal/views/index.py
@@ -1,5 +1,13 @@
 from django.views.generic.base import TemplateView
 
+from portal.models import Enrollment, Project
+
 
 class IndexView(TemplateView):
     template_name = "portal/index/index.html"
+
+    def get_context_data(self, **kwargs):
+        data =  super().get_context_data(**kwargs)
+        data["enrollment_count"] = Enrollment.objects.count()
+        data["project_count"] = Project.objects.count()
+        return data
