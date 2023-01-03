3713f82cf4dcc17364d247c9123782832348bf61
Frank Matranga <thefrankmatranga@gmail.com>
2023-01-02 22:05:44 -0500

    Add admin links to templates

--- portal/views/admin.py
+++ portal/views/admin.py
@@ -6,8 +6,10 @@ from portal.models import User, Enrollment, Semester
 from typing import TypedDict
 from django.contrib.auth.decorators import login_required, user_passes_test
 
+
 def is_admin(user):
     return user.is_superuser
+
 
 SubmittyCSVRow = TypedDict(
     "SubmittyCSVRow",
@@ -22,6 +24,7 @@ SubmittyCSVRow = TypedDict(
         "Group": str,
     },
 )
+
 
 @login_required
 @user_passes_test(is_admin)
