12b4877c9133db92360c10e4fd851c5ad91b04d7
Frank Matranga <thefrankmatranga@gmail.com>
2023-01-29 16:01:45 -0500

    Consistent naming

--- portal/views/__init__.py
+++ portal/views/__init__.py
@@ -123,7 +123,7 @@ class SearchableListView(ListView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data["search"] = ' '.join(self.request.GET.getlist("search"))
+        data["search"] = " ".join(self.request.GET.getlist("search"))
         return data
 
 
