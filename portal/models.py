efea1f7fd29f9505df4dc56c83756d01b1c1318e
Bill Ni <bill.8ni@gmail.com>
2026-02-17 01:59:30 -0500

    fix user creation bug

--- portal/models.py
+++ portal/models.py
@@ -209,7 +209,6 @@ class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
         if not email:
             raise ValueError("The given email must be set")
         email = self.normalize_email(email)
@@ -218,14 +217,12 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
+    def create_user(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
+    def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -247,6 +244,11 @@ class RPIUserManager(BaseUserManager):
             .filter(role=User.RPI, is_active=True, is_approved=True)
         )
 
+
+class User(AbstractUser, TimestampedModel):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # email is already the USERNAME_FIELD, don't list it here
+    username = None  # remove the username field
 
 class User(AbstractUser, TimestampedModel):
     """Represents an RCOS member, either an active RPI student/faculty with an RCS ID or an external user."""
